ca751ebc0082208e380fefeff65c404084f7de37
Minh Khoi (MK) <meijerink.minhkhoi@gmail.com>
2026-03-01 13:38:54 +0100

    Ensuring reproducibility

--- dc1/main_baseline.py
+++ dc1/main_baseline.py
@@ -19,7 +19,8 @@ import plotext  # type: ignore
 from datetime import datetime
 from pathlib import Path
 from typing import List
-
+# Random import for reproducibility (NEW)
+import random
 # Evaluation imports (NEW)
 import numpy as np
 from sklearn.metrics import (
@@ -29,7 +30,26 @@ from sklearn.metrics import (
     accuracy_score,
     ConfusionMatrixDisplay,
 )
+# ====== NEW: Ensuring reproducible results =====
+def deterministic_mode(seed=42):
 
+    # Set seeds for reproducibility
+    # Ensuring the order of classes cannot change in the future
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+
+    # Force cuDNN to use deterministic algorithms
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+    # Force PyTorch operations to be deterministic
+    os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"
+    torch.use_deterministic_algorithms(True)
 
 def main(args: argparse.Namespace, activeloop: bool = True) -> None:
 
@@ -228,5 +248,5 @@ if __name__ == "__main__":
         type=bool,
     )
     args = parser.parse_args()
-
+    deterministic_mode(seed=42)
     main(args)
